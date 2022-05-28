7e47bac4de55d5ba8ad368789d8b08dce88739e6
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-28 22:08:14 +0700

    information comparator

---
+++ src/com/phucdevs/set/Comparator.java
@@ -0,0 +1,19 @@
+package com.phucdevs.set;
+
+/**
+ * @author Phuc.Le
+ */
+public class Comparator {
+
+    public static void main(String[] args) {
+        informationComparator();
+    }
+
+    private static void informationComparator() {
+        System.out.println("Present in java.util.package");
+        System.out.println("Contains two methods:" +
+                                "compare(Object obj1, Object obj2)" +
+                                "equals(Object obj)");
+        System.out.println("Meant for Customized Sorting Order");
+    }
+}

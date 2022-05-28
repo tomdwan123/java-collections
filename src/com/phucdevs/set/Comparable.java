71d2babbf38d35523760bddd889e992d54b676ef
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-28 22:00:45 +0700

    information comparable

---
+++ src/com/phucdevs/set/Comparable.java
@@ -0,0 +1,17 @@
+package com.phucdevs.set;
+
+/**
+ * @author Phuc.Le
+ */
+public class Comparable {
+
+    public static void main(String[] args) {
+        informationComparable();
+    }
+
+    private static void informationComparable() {
+        System.out.println("Present in java.lang.Package");
+        System.out.println("Contains only one method compareTo(Object obj)");
+        System.out.println("Meant for Default Natural Sorting Order");
+    }
+}

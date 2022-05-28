7f4f33bfab9ebf57b2d3981fb126accbd5607de7
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-28 22:05:37 +0700

    feature comparable

--- src/com/phucdevs/set/Comparable.java
+++ src/com/phucdevs/set/Comparable.java
@@ -1,5 +1,7 @@
 package com.phucdevs.set;
 
+import java.util.TreeSet;
+
 /**
  * @author Phuc.Le
  */
@@ -7,11 +9,23 @@ public class Comparable {
 
     public static void main(String[] args) {
         informationComparable();
+        featureComparable();
     }
 
     private static void informationComparable() {
         System.out.println("Present in java.lang.Package");
         System.out.println("Contains only one method compareTo(Object obj)");
         System.out.println("Meant for Default Natural Sorting Order");
+    }
+
+    private static void featureComparable() {
+        TreeSet data = new TreeSet();
+        data.add(152);
+        data.add(185);
+        data.add(254);
+        data.add(10);
+        data.add(132);
+        data.add(10);
+        System.out.println(data);
     }
 }

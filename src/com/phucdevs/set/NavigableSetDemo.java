bba4be74e46d06f23c1360f051196f00ed378600
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-28 21:10:16 +0700

    information navigable set

---
+++ src/com/phucdevs/set/NavigableSetDemo.java
@@ -0,0 +1,18 @@
+package com.phucdevs.set;
+
+/**
+ * @author Phuc.Le
+ */
+public class NavigableSetDemo {
+
+    public static void main(String[] args) {
+        informationNavigableSet();
+    }
+
+    private static void informationNavigableSet() {
+        System.out.println("Sub interface of SortedSet");
+        System.out.println("Contains methods related to Navigation functionality");
+        System.out.println("Both SortedSet and NavigableSet have a same " +
+                            "implementation class as TreeSet");
+    }
+}

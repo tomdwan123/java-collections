ffcb193273b52c830b19e5b2c0a95084e5280c8c
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-28 10:31:26 +0700

    variables assignment

--- src/com/phucdevs/array/ArrayMain.java
+++ src/com/phucdevs/array/ArrayMain.java
@@ -6,7 +6,8 @@ package com.phucdevs.array;
 public class ArrayMain {
 
     public static void main(String[] args) {
-        elementsAssign();
+        //elementsAssign();
+        variablesAssign();
     }
 
     static void elementsAssign() {
@@ -31,5 +32,32 @@ public class ArrayMain {
         runnables[0] = new Thread();
         System.out.println(runnables[0]); // Thread[Thread-0,5,main]
         System.out.println(runnables[1]); // null
+    }
+
+    static void variablesAssign() {
+        int[] x = new int[2];
+        int[] a = x;
+        System.out.println(x[0]); // 0
+        System.out.println(a[0]); // 0
+
+        int[] x2 = new int[2];
+        int[] a2 = x2;
+        x2[0] = 1;
+        System.out.println(x2[0]); // 1
+        System.out.println(a2[0]); // 1
+        a2[1] = 2;
+        System.out.println(x2[1]); // 2
+        System.out.println(a2[1]); // 2
+
+        int[] b = {1, 2, 3};
+        int[] c = {11, 22};
+        b = c;
+        for (int i : b) // 2 11 22
+            System.out.println(i);
+        for (int i : c) // 11 22
+            System.out.println(i);
+
+         String[] s = new String[3];
+         Object[] o = s;
     }
 }

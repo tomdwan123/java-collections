69f10543214c04a1caaf78fde557ad8582b32902
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-28 14:28:42 +0700

    information hashset

--- src/com/phucdevs/collections/CollectionsSetMain.java
+++ src/com/phucdevs/collections/CollectionsSetMain.java
@@ -21,6 +21,19 @@ public class CollectionsSetMain {
     }
 
     private static void hashSet() {
+        informationHashSet();
+        featureHashSet();
+    }
+
+    private static void informationHashSet() {
+        System.out.println("Does not allow duplicates");
+        System.out.println("Insertion order is not preserved");
+        System.out.println("Can add heterogeneous elements");
+        System.out.println("We can add null value");
+        System.out.println("Implements serializable and cloneable interface");
+    }
+
+    private static void featureHashSet() {
 
     }
 }

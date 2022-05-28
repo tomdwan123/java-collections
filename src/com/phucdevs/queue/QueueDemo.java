6969101158c784a4ff699b0e16d6258518d78466
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-28 22:20:33 +0700

    information queue

---
+++ src/com/phucdevs/queue/QueueDemo.java
@@ -0,0 +1,20 @@
+package com.phucdevs.queue;
+
+/**
+ * @author Phuc.Le
+ */
+public class QueueDemo {
+
+    public static void main(String[] args) {
+        informationQueue();
+    }
+
+    private static void informationQueue() {
+        System.out.println("Queue");
+        System.out.println("- Linked List");
+        System.out.println("- Priority Queue");
+        System.out.println("- Blocking Queue" +
+                                "- Priority Blocking Queue" +
+                                "- Linked Blocking Queue");
+    }
+}

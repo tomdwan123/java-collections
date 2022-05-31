d516418958238b7a73df3971304688947b0510d8
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-31 13:46:06 +0700

    Introduction set interface demo

---
+++ src/com/phucdevs/map/MapDemo.java
@@ -0,0 +1,17 @@
+package com.phucdevs.map;
+
+/**
+ * @author Phuc.Le
+ */
+public class MapDemo {
+
+    public static void main(String[] args) {
+        informationMapInterface();
+    }
+
+    private static void informationMapInterface() {
+        System.out.println("Keys and values are objects, can be of any data type");
+        System.out.println("Duplicate keys are not allowed, values can be duplicated");
+        System.out.println("Collection of entry-objects");
+    }
+}

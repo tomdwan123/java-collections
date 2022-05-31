072f7ca113b028ca5e2b3a19ccb24b9f4e64b045
Phuc Tan Le <phuc.lt@ecom365.vn>
2022-05-31 09:12:47 +0700

    check duplicate  item in number array

--- src/com/phucdevs/hashtable/HashSetDemo.java
+++ src/com/phucdevs/hashtable/HashSetDemo.java
@@ -1,6 +1,8 @@
 package com.phucdevs.hashtable;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Phuc.Le
@@ -49,5 +51,30 @@ public class HashSetDemo {
         hashSetDemo.add(1);
         hashSetDemo.remove(1);
         hashSetDemo.contains(2);
+
+        int[] numbers1 = {1, 2, 1};
+        int[] numbers2 = {1, 2, 3};
+        System.out.println(checkDuplicateNumbers(numbers1));
+        System.out.println(checkDuplicateNumbers(numbers2));
+    }
+
+    /**
+     * Input: Cho 1 danh sách các số tự nhiên
+     * Output:
+     *      + Trả về true nếu tồn tại bất kỳ phần tử
+     *      nào có số lần xuất hiện >= 2
+     *      + Trả về false nếu tất cả phần tử đều distinct
+     */
+    private static boolean checkDuplicateNumbers(int[] numbers) {
+        Set<Integer> setNumbers = new HashSet<>();
+        for (int number : numbers) {
+            if (setNumbers.contains(number)) {
+                return true;
+            } else {
+                setNumbers.add(number);
+            }
+        }
+
+        return false;
     }
 }
